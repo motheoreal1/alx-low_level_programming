901f8ccdaab94f01307e372fcc4a8d13889af733
motheoreal1 <motheo22motimela32@gmail.com>
2023-06-18 14:59:39 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,9 +24,12 @@ else if (lastd == 0)
 {
 printf("Last digit of %d is %d and is 0\n", n, lastd);
 }
-else (lastd < 6 && lastd != 0)
+else
+
+(lastd < 6 && lastd != 0)
 {
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
 }
 return (0);
-}	
+}
+
